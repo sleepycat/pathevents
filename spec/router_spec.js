989d4dc996ee782725e39440b7551f8d819b12f6
Mike Williamson <mike@korora.ca>
2013-11-14 23:46:38 -0500

    Router can now save routes.

--- spec/router_spec.js
+++ spec/router_spec.js
@@ -1,7 +1,34 @@
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      expect([1, 2, 3].indexOf(5)).toEqual(-1);
+describe('router.js', function(){
+
+  describe('router', function(){
+
+    it('creates a global var named router', function(){
+      expect(typeof(router)).toEqual('object');
     });
+
+    it('creates regex for matching routes', function(){
+      expect(router.createMatchString("foo/:id")).toEqual('foo\/(.+)');
+    });
+
+    it('pulls out dynamic segments from the path spec', function(){
+      expect(router.getSegments('foo/:id')).toEqual(['id']);
+    });
+
+    it('can deal with multiple segments', function(){
+      expect(router.getSegments('foo/:bar/:baz')).toEqual(['bar', 'baz']);
+    });
+
+    it('parses path specs to create routes', function(){
+      router.register({
+        foo: "foo/:bar"
+      });
+
+      expected = {};
+      expected["foo\/(.+)"] = {};
+      expected["foo\/(.+)"].foo= ['bar'];
+      expect(router.routes).toEqual(expected);
+    });
+
   });
+
 });
--- src/router.js
+++ src/router.js
@@ -1,33 +1,32 @@
-//var path = window.location.pathname;
-
-var router = {};
-router.routes = [];
-
 //router fires onload and tries to match
 //gives 404 or whatever if no route matches
 //listens for hashchange event and tries to match
 
-// remove leading slash
-// escape rest of the slashes
-// sub in (.+)
-// save as regex
-// 'foo/:id/:bar'.match(/:\w+/g)
-// [":id", ":bar"]
-// 'foo/1/baz'.match(/foo\/(.+)\/(.+)/)
-// ["foo/1/baz", "1", "baz"]
+var router = {};
+router.routes = {};
 
-router.register = function(route_collection){
-  for(var route_name in route_collection){
-    if(route_collection.hasOwnProperty(route_name)){
-      router.routes[route_name] = route_collection[route_name].replace(/:\w+/, '(.+)');
+router.createMatchString = function(pathSpec){
+  var slashesEscaped = pathSpec.replace('/', '\/');
+  var matchString = slashesEscaped.replace(/:\w+/g, '(.+)');
+  return matchString;
+};
+
+router.getSegments = function(pathSpec){
+  var segments = pathSpec.match(/:\w+/g);
+  for(var count=0, length = segments.length; count < length; count++) {
+     segments[count] = segments[count].replace(/:/g, '');
+  }
+  return segments;
+};
+
+router.register = function(routeCollection){
+  for(var routeName in routeCollection){
+    if(routeCollection.hasOwnProperty(routeName)){
+      var matchString = this.createMatchString(routeCollection[routeName]);
+      var segments = this.getSegments(routeCollection[routeName]);
+      router.routes[matchString] = {};
+      router.routes[matchString][routeName]= segments;
+      return router.routes;
     }
   }
 };
-
-router.recognize = function(pathname){
-  
-};
-
-router.register({
-  foo: "foo/:id"
-});
